919961f3b2e2b3a712b5c15e9c1161b21f74485f
Eddy Manuel <eddymanuel1998@outlook.es>
2025-02-23 03:36:44 -0300

    adding sections y validations

--- Formulario.js
+++ Formulario.js
@@ -1,39 +1,64 @@
 
 function iniciarFormulario(){
-    
-    document.getElementById("divUltimosJuegosJugados").style.display="none"
-    document.getElementById("divJuegosQuePlaneesJugar").style.display="none"
-    document.getElementById("divJuegoFavorito").style.display="none"
-    document.getElementById("divJuegosAnime").style.display="none"
+    document.getElementById("sectionDatosPersonales").style.display=""
+    document.getElementById("sectionUltimosJuegosJugados").style.display="none"
+    document.getElementById("sectionJuegosQuePlaneesJugar").style.display="none"
+    document.getElementById("sectionJuegoFavorito").style.display="none"
+    document.getElementById("sectionJuegosAnime").style.display="none"
     document.getElementById("divListaJuegosAnime").style.display="none"
     document.getElementById("textarea").style.display="none"
     
     document.getElementById("Formulario").addEventListener("click", siguiente)
 }   
 
+function cambiarSeccion(seccionActual, seccionSiguiente){
+    document.getElementById(seccionActual).style.display = "none"
+    document.getElementById(seccionSiguiente).style.display = ""
+}
+
 function siguiente(evento){
-    if (evento.target.matches("#botonSiguiente1")){
-        document.getElementById("divDatosPersonales").style.display= "none"
-        document.getElementById("divUltimosJuegosJugados").style.display = ""
-    }
+    let secciones = ["sectionDatosPersonales", "sectionUltimosJuegosJugados", "sectionJuegosQuePlaneesJugar", "sectionJuegoFavorito", "sectionJuegosAnime", "divListaJuegosAnime"]
+    let seccionVisible = secciones.find(id => document.getElementById(id).style.display !=="none")
+    let inputsVisibles = document.querySelectorAll(`#${seccionVisible} input, #${seccionVisible} select, #${seccionVisible} textarea`)
+ 
+    if(evento.target.matches("#botonSiguiente1")){ 
+        for (let input of inputsVisibles){
+            if(!input.checkValidity()){
+                alert("Please fill out all the fields");
+                return;
+            }
+        }
+        cambiarSeccion("sectionDatosPersonales", "sectionUltimosJuegosJugados")
+    } 
     else if(evento.target.matches("#botonSiguiente2")){
-        document.getElementById("divUltimosJuegosJugados").style.display = "none"
-        document.getElementById("divJuegosQuePlaneesJugar").style.display = ""
+        let juegosJugados = document.querySelectorAll("#sectionUltimosJuegosJugados input[type='checkbox']:checked")
+        if (juegosJugados.length < 2){
+            alert("Please select at least 2 games")
+            return;
+        }
+        else{
+            cambiarSeccion("sectionUltimosJuegosJugados", "sectionJuegosQuePlaneesJugar")
+        }
     }
     else if(evento.target.matches("#botonSiguiente3")){
-        document.getElementById("divJuegosQuePlaneesJugar").style.display = "none"
-        document.getElementById("divJuegoFavorito").style.display = ""
+        let juegosPorJugar = document.querySelectorAll("#sectionJuegosQuePlaneesJugar input[type='checkbox']:checked")
+        if (juegosPorJugar.length < 2){
+            alert("Please select at least 2 games")
+            return;
+        }
+        else{
+            cambiarSeccion("sectionJuegosQuePlaneesJugar", "sectionJuegoFavorito")
+        }
     }
     else if(evento.target.matches("#botonSiguiente4")){
-        document.getElementById("divJuegoFavorito").style.display = "none"
-        document.getElementById("divJuegosAnime").style.display = ""
+        cambiarSeccion("sectionJuegoFavorito", "sectionJuegosAnime")
     }
     else if(evento.target.matches("#siAnime")){
-         divListaJuegosAnime.style.display=""
+        document.getElementById("divListaJuegosAnime").style.display=""
     }
     else if(evento.target.matches("#noAnime")){
-        divListaJuegosAnime.style.display="none"
-   }
+        document.getElementById("divListaJuegosAnime").style.display="none"
+    }
     else if(evento.target.matches("#selectListaDeJuegosAnime")){
         if (selectListaDeJuegosAnime.value == "otro"){
             textarea.style.display=""
@@ -41,6 +66,7 @@ function siguiente(evento){
         else{
             textarea.style.display="none"
         }
-   }
+    }
 }
+
 window.addEventListener("load", iniciarFormulario)
